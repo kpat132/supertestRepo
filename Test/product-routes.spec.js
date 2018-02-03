3b7ce93f432f121935712feb281db8257ca7c4ce
KrishnaP <kpat132@gmail.com>
2018-02-02 15:27:21 -1000

    stuff

--- Test/product-routes.spec.js
+++ Test/product-routes.spec.js
@@ -6,4 +6,12 @@ const app = require('../server');
 
 describe('GET product smoke test', function(){
 
+  it('should return "product smoke test" text', function(){
+    request(app)
+    .get('/products')
+    .expect(200)
+    .expect('Content-Type', /text/)
+    .end(done);
+  })
+
 });
