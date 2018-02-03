c6144d627b3b88a82b4acb6f448b2d546d4be523
KrishnaP <kpat132@gmail.com>
2018-02-02 15:20:10 -1000

    k

--- Test/product-routes.spec.js
+++ Test/product-routes.spec.js
@@ -1,3 +1,9 @@
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
+
+const app = require('../server');
+
+describe('GET product smoke test', function(){
+
+});
